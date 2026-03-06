e4b0f9b348726667f58c2f841092fbefccc9a8ab
Astha Bhatia <astha18bhatia@gmail.com>
2026-03-06 09:57:20 +0530

    UC2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,71 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 
+abstract class Room {
+    protected String roomType;
+    protected int beds;
+    protected int size;
+    protected double price;
+
+    public Room(String roomType, int beds, int size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Size: " + size + " sq ft");
+        System.out.println("Price per night: ₹" + price);
+    }
+}
+
+class SingleRoom extends Room {
+    public SingleRoom() {
+        super("Single Room", 1, 200, 2500);
+    }
+}
+
+class DoubleRoom extends Room {
+    public DoubleRoom() {
+        super("Double Room", 2, 350, 4000);
+    }
+}
+
+// Suite Room class
+class SuiteRoom extends Room {
+    public SuiteRoom() {
+        super("Suite Room", 3, 600, 7500);
+    }
+}
+
+// Main class
 public class Main {
 
-    /**
-     * Main method - Entry point of the Java application
-     * @param args Command line arguments
-     */
     public static void main(String[] args) {
 
         System.out.println("Welcome to Book My Stay - Hotel Booking System");
+        System.out.println("Application Version: v2.1\n");
 
-        System.out.println("Application Version: v1.0");
+        Room single = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suite = new SuiteRoom();
 
-        System.out.println("System initialized successfully. Thank you for using Book My Stay!");
+        int singleAvailability = 10;
+        int doubleAvailability = 7;
+        int suiteAvailability = 3;
+
+        System.out.println("----- Room Details -----\n");
+
+        single.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleAvailability + "\n");
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleAvailability + "\n");
+
+        suite.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteAvailability + "\n");
+
+        System.out.println("Thank you for using Book My Stay!");
     }
 }
